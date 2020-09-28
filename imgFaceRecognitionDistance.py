347e1404075b19426f3895bc395e122a60296e6d
ZSabakh <zurabsabakhtarishvili@gmail.com>
2020-09-27 21:34:47 -0500

    Can recognize faces using pre-generated datasets

--- imgFaceRecognitionDistance.py
+++ imgFaceRecognitionDistance.py
@@ -4,27 +4,56 @@
 """
 
 import cv2
-import dlib
 import face_recognition
+from numpy import load
 
-image_detect_path = "images/misha.jpg"
+import time
 
-starting_image = cv2.imread(image_detect_path)
+start = time.time()
+all_file_names = load('dataset/names.npy')
+all_face_encodings = load('dataset/encodings.npy')
 
-misha_image = face_recognition.load_image_file('images/misha.jpg')
-misha_face_encodings = face_recognition.face_encodings(misha_image)[0]
+def roundToPercentage(x):
+    x = round(((1-float(x))*100),2)
+    return x
 
-bidzina_image = face_recognition.load_image_file('images/bidzina.jpg')
-bidzina_face_encodings = face_recognition.face_encodings(bidzina_image)[0]
+image_to_scan = "images/mishabidz.jpg"
+
+starting_image = cv2.imread(image_to_scan)
 
 
-all_face_encodings = [misha_face_encodings, bidzina_face_encodings]
-all_face_names = ["Mikheil Saakashvili", "Bidzina Ivanishvili"]
+image_to_detect = face_recognition.load_image_file(image_to_scan)
+faces_locations = face_recognition.face_locations(image_to_detect, model="hog", number_of_times_to_upsample=2)
 
-image_to_detect = face_recognition.load_image_file(image_detect_path)
-image_to_detect_encodings = face_recognition.face_encodings(image_to_detect)[0]
-face_distances = face_recognition.face_distance(all_face_encodings, image_to_detect_encodings)
+for x in range(len(faces_locations)):
+    faces_encodings = face_recognition.face_encodings(image_to_detect, faces_locations, num_jitters=3, model="small")[x]
+    face_distances = face_recognition.face_distance(all_face_encodings, faces_encodings)
+
+
+
+name_index = {}
 
 for i,face_distance in enumerate(face_distances):
-    print("Face distance is {} from {}".format(face_distance,all_face_names[i]))
-    print("Matches {}% to {}".format(round(((1-float(face_distance))*100),2),all_face_names[i]))
+    name_index.update( {all_file_names[i] : face_distance} )
+
+
+name_index = {k: v for k, v in sorted(name_index.items(), key=lambda item: item[1])}
+
+
+
+for one_face_location, one_face_encoding in zip(faces_locations, faces_encodings):
+    top_pos, right_pos, bottom_pos, left_pos = one_face_location
+    cv2.rectangle(starting_image, (left_pos, top_pos), (right_pos, bottom_pos), (255,0,0), 2)
+
+font = cv2.FONT_HERSHEY_DUPLEX
+
+for x in range(len(faces_locations)):
+    print('Probability: {}% Name: {}'.format(roundToPercentage(list(name_index.values())[x]),list(name_index.keys())[x]))
+
+name = list(name_index.keys())[0]
+end = time.time()
+print('Took {} seconds to compute'.format(end - start))
+
+cv2.putText(starting_image, name, (left_pos, bottom_pos), font, 1, (0,0,0), 1)
+cv2.imshow("Detekt", starting_image)
+cv2.waitKey(0)
